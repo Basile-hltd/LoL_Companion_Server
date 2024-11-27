698cc935da69e0b909f96a2ad7bd8b6f3b0b25bf
Basile Hiltpold <basile@hiltpold.org>
2024-11-27 13:34:32 +0100

    FirtServerTest

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import socket
 
 if __name__ == "__main__":
-    print("Hello Wolrd")
+    print("===== Server Starting =====")
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server_socket.bind(("0.0.0.0", 50005))
+
+    server_socket.listen(5)
+
+    while True:
+        client_socket, client_addr = server_socket.accept()
+
+        data = client_socket.recv(1024).decode("ascii")
+        print(data)
